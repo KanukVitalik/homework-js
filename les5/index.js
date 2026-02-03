6945eaa3b0db4ac8d59edbf68bb8216e5e0d8d93
Vitalik <kanukvitalik@gmail.com>
2026-02-03 15:07:45 +0200

    commit les5

--- les5/index.js
+++ les5/index.js
@@ -7,6 +7,8 @@ function compare(a, b) {
     return 0;
 }
 
+compare(2,4)
+
 // 2
 function factorial(n) {
     let result = 1;
@@ -16,16 +18,20 @@ function factorial(n) {
     return result;
 }
 
+factorial(4)
+
 // 3
 function digitsToNumber(a, b, c) {
     return (`${a}${b}${c}`);
 }
 
+digitsToNumber(2,4,6)
 // 4
 function rectangleArea(a, b = a) {
     return a * b;
 }
 
+rectangleArea(2,5)
 // 5
 function isPerfect(num) {
     let sum = 0;
@@ -36,6 +42,8 @@ function isPerfect(num) {
     return sum === num && num !== 0;
 }
 
+isPerfect(20)
+
 // 6
 function perfectInRange(min, max) {
     for (let i = min; i <= max; i++) {
@@ -45,7 +53,11 @@ function perfectInRange(min, max) {
     }
 }
 
+perfectInRange(20 , 34)
+
 // 8
 function timeToSeconds(h, m, s) {
     return h * 3600 + m * 60 + s;
 }
+
+timeToSeconds(18,38,23)
