274f3a0d3a27529379520071c66d3e9081704a43
Vitalik <kanukvitalik@gmail.com>
2026-02-21 09:40:16 +0200

    commit map

---
+++ map/index.js
@@ -0,0 +1,35 @@
+// 1
+// let staff = [
+//     {id : 1 , name : 'bread' , price : 500 , isActive : true , discound : false},
+//     {id : 2 , name : 'phone' , price : 1500 , isActive : true , discound : true},
+//     {id : 3 , name : 'PC' , price : 2000 , isActive : false , discound : false},
+//     {id : 4 , name : 'TV' , price : 1500 , isActive : true , discound : true}
+// ]
+// 2
+// let moreTo15pr = staff.map(obj => obj.price * 1.15);
+// console.log(moreTo15pr);
+
+// 3
+// let moreThan1000 = staff.filter(obj => obj.price > 1000);
+// console.log(moreThan1000);
+
+// 4
+// let idFind = staff.find(obj => obj.id === 1);
+// console.log(idFind);
+
+// 5
+// let allActive = staff.every(obj => obj.isActive === true);
+// console.log(allActive);
+
+// 6
+// let atLeastOneDisc = staff.some(obj => obj.discound === true);
+// console.log(atLeastOneDisc);
+
+// 7
+// let prices = staff.map(obj => obj.price);
+// console.log(prices);
+
+// 8
+// let biggestPrice = Math.max(...prices);
+// console.log(staff.find(obj => obj.price === biggestPrice));
+
